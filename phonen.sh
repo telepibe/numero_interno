dae880b9d4207e46bff343f5d229d1e4d43cb054
Juani <2912602+telepibe@users.noreply.github.com>
2019-08-22 15:58:08 -0300

    First commit

---
+++ phonen.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# $search defaults to empty string if its not defined.
+search=${1:-}
+
+if [[ -z $search ]]; then
+	echo "Uso: $0 nombre|interno"
+	exit 1
+fi
+
+# Loads $tels array with lines of file. Keeps newlines.
+mapfile tels <db.txt
+
+# $search in caps and captures int number (not needed actually)
+regex=".*${search^^}.*([0-9]{4})"
+
+for interno in "${tels[@]}"; do
+	[[ ${interno^^} =~ $regex ]] && echo "${BASH_REMATCH[0]}"
+done
